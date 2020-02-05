135f16282ed87348055c06ee7d87e24b94e1097c
Nashmia Riaz <nashmia-riaz@hotmail.com>
2020-02-05 14:03:01 +0000

    Added web build

--- Assets/Scripts/Recipe.cs
+++
@@ -1,7 +0,0 @@
-﻿//recipe class,contains array of ingredients as string
-public class Recipe 
-{
-    public string[] ingredients;
-
-    public Recipe(string[] new_ingredients) { ingredients = new_ingredients; }
-}
